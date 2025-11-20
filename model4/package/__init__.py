386750a9bee72b53a8f265643220382b0b769db1
Marcus <22012026d@connect.polyu.hk>
2025-11-20 12:41:04 +0800

    updated init.py in model4

--- model4/package/__init__.py
+++ model4/package/__init__.py
@@ -2,3 +2,5 @@ from .decision_tree_model import DecisionTreeSentiment
 from .preprocess import clean_text
 from .vectorizers import get_count_vectorizer
 from .data_loader import load_dataset
+
+__all__ = ["DecisionTreeSentiment", "clean_text", "get_count_vectorizer", "load_dataset"]
