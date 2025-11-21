06d23cbfd22cbae28635ac272f5277f6aec59cbf
SoChingHang <137737068+SoChingHang@users.noreply.github.com>
2025-11-21 22:12:20 +0800

    Update __init__.py

--- model4/package/__init__.py
+++ model4/package/__init__.py
@@ -1,6 +1,12 @@
-from .decision_tree_model import DecisionTreeSentiment
+from .decision_tree_model import DecisionTreeSentiment, load_saved_model
 from .preprocess import clean_text
 from .vectorizers import get_count_vectorizer
 from .data_loader import load_dataset
 
-__all__ = ["DecisionTreeSentiment", "clean_text", "get_count_vectorizer", "load_dataset"]
+__all__ = [
+    "DecisionTreeSentiment",
+    "load_saved_model",
+    "clean_text",
+    "get_count_vectorizer",
+    "load_dataset"
+]
