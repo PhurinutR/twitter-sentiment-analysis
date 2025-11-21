dabb8e4c30266b85398b056131b27e8433d8b177
SoChingHang <137737068+SoChingHang@users.noreply.github.com>
2025-11-21 22:12:46 +0800

    Update data_loader.py

--- model4/package/data_loader.py
+++ model4/package/data_loader.py
@@ -4,10 +4,10 @@ def load_dataset(train_path, test_path, text_col="text", label_col="label"):
     train_df = pd.read_csv(train_path)
     test_df = pd.read_csv(test_path)
 
-    X_train = train_df[text_col]
-    y_train = train_df[label_col]
+    X_train = train_df[text_col].tolist()
+    y_train = train_df[label_col].tolist()
 
-    X_test = test_df[text_col]
-    y_test = test_df[label_col]
+    X_test = test_df[text_col].tolist()
+    y_test = test_df[label_col].tolist()
 
     return X_train, y_train, X_test, y_test
