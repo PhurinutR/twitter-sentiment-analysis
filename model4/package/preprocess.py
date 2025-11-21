9121224be63b33f84ddf5eac4998928283539cfb
SoChingHang <137737068+SoChingHang@users.noreply.github.com>
2025-11-21 22:13:07 +0800

    Update preprocess.py

--- model4/package/preprocess.py
+++ model4/package/preprocess.py
@@ -1,6 +1,9 @@
 import re
 
 def clean_text(text):
+    if not isinstance(text, str):
+        text = str(text)
+
     text = text.lower()
     text = re.sub(r"[^a-z0-9\s]", " ", text)
     text = re.sub(r"\s+", " ", text).strip()
